d567fa48a3cf907c6a288a99b5b6a19074b6ebbc
Leonard Garvey <lengarvey@gmail.com>
2013-06-22 01:59:32 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Writedown
+
+Write some markdown down.
+
+Simple Rails app that does Markdown authoring well.
+
+Built for Railscamp Melbourne 2013.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Writedown::Application.config.secret_token = 'ff202c0aa2b551fc2a2df98a1592d4f852aa5a13072dfdeb9ca0e087320e91f5c6881fa49ab8a2c755157e6ee260725f542571fef027c768949ba004de9dd3b9'
